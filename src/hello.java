bb380901af7e60617cd7f860bb527f968156b23b
Prathap Mohan <prathapmohan@Prathaps-MacBook-Pro.local>
2020-10-21 11:38:34 +0530

    second commit from local

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,7 @@
 public class hello {
    public static void main(String []args) {
 
-      System.out.println("hello world 123456");
+      System.out.println("hello world");
       System.out.println("My Name is Prathap and Akhile");
 
   }
