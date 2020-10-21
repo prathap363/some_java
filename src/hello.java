dd31d4eef8c88b1415cea6c88db0970733e1c071
prathap363 <58847249+prathap363@users.noreply.github.com>
2020-10-21 11:56:34 +0530

    Update hello.java
    Ui first commit

--- src/hello.java
+++ src/hello.java
@@ -4,7 +4,7 @@ public class hello {
       System.out.println("hello world");
       System.out.println("My Name is Prathap and Akhile");
 
-      System.out.println("I dont like you ");
+      System.out.println("I always love you");
   }
 
 }
