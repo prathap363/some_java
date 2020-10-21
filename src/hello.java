327ad2ecb9e455e50b13c9ff8fcdc62a24397c77
prathap363 <58847249+prathap363@users.noreply.github.com>
2020-10-21 12:07:34 +0530

    Ui5

--- src/hello.java
+++ src/hello.java
@@ -4,12 +4,13 @@ public class hello {
       System.out.println("hello world");
       System.out.println("hello world 1);
       System.out.println("My Name is Prathap and Akhile");
-
-   UI1
-                         UI2
-                         
-                         UI3
+      
       System.out.println("I always love you");
+                           System.out.println("1");
+                           System.out.println("2");
+                           System.out.println("3";
+                           System.out.println("4");
+                           System.out.println("5");
 
   }
 
