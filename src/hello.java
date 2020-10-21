8c7b5c962ac5d58ab18668904c4a7cb74e27a190
prathap363 <58847249+prathap363@users.noreply.github.com>
2020-10-21 11:09:59 +0530

    Update hello.java
    Direct UI change

--- src/hello.java
+++ src/hello.java
@@ -1,9 +1,7 @@
 public class hello {
    public static void main(String []args) {
-<<<<<<< HEAD
-      System.out.println("hello world 3");
-=======
-      System.out.println("hello world 11");
->>>>>>> second commit
+
+      System.out.println("hello world");
+
    }
 }
