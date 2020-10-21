cead6f4bbaa47c5eed28f12063b4f1b826cd7d94
Prathap Mohan <prathapmohan@Prathaps-MacBook-Pro.local>
2020-10-21 11:12:49 +0530

    local edit

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,8 @@
 public class hello {
    public static void main(String []args) {
 
-      System.out.println("hello world");
+      System.out.println("hello world 1");
       System.out.println("My Name is Prathap");
    }
+
 }
