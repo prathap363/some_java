115b9c42f403ec428b04427cfe4bcadce386fece
prathap363 <58847249+prathap363@users.noreply.github.com>
2020-10-21 13:03:53 +0530

    test in UI

--- src/hello.java
+++ src/hello.java
@@ -3,15 +3,15 @@ public class hello {
 
       System.out.println("hello world");
       System.out.println("hello world 1);
-      System.out.println("My Name is Prathap and Akhile");
+      System.out.println("My Name is Prathap and Akhile Nothing");
       
-      System.out.println("I always love you");
+     
                            System.out.println("1");
                            System.out.println("2");
                            System.out.println("3";
                            System.out.println("4");
                            System.out.println("5");
-                                                 System.out.println("6");
+                           System.out.println("6");
 
   }
 
