6ce6cc504c6e4515a3a80c2f4b91e528657bab5c
prathap363 <58847249+prathap363@users.noreply.github.com>
2020-10-21 13:08:51 +0530

    UI fix

--- src/hello.java
+++ src/hello.java
@@ -11,7 +11,7 @@ public class hello {
                            System.out.println("3";
                            System.out.println("4");
                            System.out.println("5");
-                           System.out.println("6");
+                           System.out.println("7");
 
   }
 
