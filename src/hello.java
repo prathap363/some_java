38a40d14730da8fea7081f1abd3d51b0f2d698c1
prathap363 <58847249+prathap363@users.noreply.github.com>
2020-10-21 11:21:16 +0530

    Update hello.java
    UI edit

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,7 @@
 public class hello {
    public static void main(String []args) {
 
-      System.out.println("hello world 1");
+      System.out.println("hello world 123");
       System.out.println("My Name is Prathap");
    }
 
