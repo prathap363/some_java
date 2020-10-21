f73e6d8b7605e2f3963284562928d9b15fe97c9d
prathap363 <58847249+prathap363@users.noreply.github.com>
2020-10-21 11:27:40 +0530

    Update hello.java
    UI final

--- src/hello.java
+++ src/hello.java
@@ -2,7 +2,7 @@ public class hello {
    public static void main(String []args) {
 
 
-      System.out.println("hello world 123");
+      System.out.println("hello world 123456");
       System.out.println("My Name is Prathap and Akhile");
 
   }
